4258e171a514aea19734321fab0ffc6ec390aead
siyam-hosssain <siyam.cybersoul@gmail.com>
2025-12-30 23:34:37 +0600

    Module_142_Inline_Banner: 1

---
+++ Module_143_Inline_Collapsible_Banner_Ads/app/src/main/java/com/example/module_143_inline_collapsible_banner_ads/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.module_143_inline_collapsible_banner_ads;
+
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.widget.FrameLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    FrameLayout ad_view_container;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        initialize();
+        loadBanner();
+
+
+
+    }
+
+
+    public void initialize(){
+        new Thread(
+                () -> {
+                    // Initialize the Google Mobile Ads SDK on a background thread.
+                    MobileAds.initialize(this, initializationStatus -> {});
+                })
+                .start();
+    }
+    public AdSize getAdSize(){
+
+//        Display display = getWindowManager().getDefaultDisplay();
+//        DisplayMetrics outMetrics = new DisplayMetrics();
+//        display.getMetrics(outMetrics);
+//
+//        float widthPixels = outMetrics.widthPixels;
+//        float density = outMetrics.density;
+//
+//        int adWidth = (int) (widthPixels/density);
+//
+//        return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(this, adWidth);
+
+
+
+//        adView.setAdSize(AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(this, 360));
+        return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(this, 360);
+
+    }
+    public void loadBanner(){
+
+        ad_view_container = findViewById(R.id.ad_view_container);
+
+        AdView adView = new AdView(this);
+        adView.setAdUnitId(getString(R.string.banner_ads));
+        adView.setAdSize(getAdSize());
+
+        ad_view_container.removeAllViews();
+        ad_view_container.addView(adView);
+
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+    }
+
+}
---
+++ Module_143_Inline_Collapsible_Banner_Ads/app/src/test/java/com/example/module_143_inline_collapsible_banner_ads/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.module_143_inline_collapsible_banner_ads;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
