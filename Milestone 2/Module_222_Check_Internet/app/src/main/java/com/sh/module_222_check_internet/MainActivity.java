b9178fbd3d4d4e6b73b5be9b634db47420b4c06a
siyam-hosssain <siyam.cybersoul@gmail.com>
2026-01-26 23:37:24 +0600

    Module 222: check internet

---
+++ Milestone 2/Module_222_Check_Internet/app/src/main/java/com/sh/module_222_check_internet/MainActivity.java
@@ -0,0 +1,66 @@
+package com.sh.module_222_check_internet;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    TextView tvInternet;
+
+    //manifest modified
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        btn = findViewById(R.id.btn);
+        tvInternet = findViewById(R.id.tvInternet);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+                if(networkInfo != null && networkInfo.isConnectedOrConnecting()){
+                    tvInternet.setText("Internet available");
+                }else{
+                    tvInternet.setText("");
+//                    tvInternet.setText("No Internet connected");
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setTitle("No Internet")
+                            .setMessage("Please Connect your network")
+                            .show();
+                }
+
+
+            }
+        });
+
+
+    }
+}
