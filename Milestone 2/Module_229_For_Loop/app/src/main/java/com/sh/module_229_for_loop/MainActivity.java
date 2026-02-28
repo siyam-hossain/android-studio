908db5169e7f34c5b6aec285a7b213213f002c2e
siyam-hosssain <siyam.cybersoul@gmail.com>
2026-02-28 21:21:39 +0600

    Module_229

---
+++ Milestone 2/Module_229_For_Loop/app/src/main/java/com/sh/module_229_for_loop/MainActivity.java
@@ -0,0 +1,48 @@
+package com.sh.module_229_for_loop;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    LinearLayout mylayout;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        mylayout = findViewById(R.id.myLayout);
+
+
+        for (int x = 1; x <= 100; x++){
+
+            Button btn = new Button(MainActivity.this);
+            btn.setText("Button"+x);
+
+
+
+            mylayout.addView(btn);
+
+        }
+
+
+    }
+}
