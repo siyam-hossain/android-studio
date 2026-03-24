b818a2a976ad864a8fa85dba1245c11a8748d6f3
siyam-hosssain <siyam.cybersoul@gmail.com>
2026-03-24 12:45:29 +0600

    Module 246: Grid View

---
+++ Milestone 2/Module_246_GridView/app/src/main/java/com/sh/module_246_gridview/MainActivity.java
@@ -0,0 +1,75 @@
+package com.sh.module_246_gridview;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    GridView gridView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        gridView = findViewById(R.id.gridView);
+
+        MyAdapter adapter = new MyAdapter();
+        gridView.setAdapter(adapter);
+
+
+
+
+    }
+
+    public class MyAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return 10;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup viewGroup) {
+
+            LayoutInflater layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View myView = layoutInflater.inflate(R.layout.item_card, viewGroup, false);
+
+
+
+            return myView;
+        }
+    }
+
+
+}
