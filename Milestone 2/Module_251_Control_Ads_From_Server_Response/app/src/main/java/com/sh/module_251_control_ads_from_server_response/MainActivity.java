f5ea010e5a6b31e461d86081472e135d6fb247da
siyam-hosssain <siyam.cybersoul@gmail.com>
2026-03-27 12:03:00 +0600

    Module 251: Control ads from server response update

--- Milestone 2/Module_251_Control_Ads_From_Server_Response/app/src/main/java/com/sh/module_251_control_ads_from_server_response/MainActivity.java
+++ Milestone 2/Module_251_Control_Ads_From_Server_Response/app/src/main/java/com/sh/module_251_control_ads_from_server_response/MainActivity.java
@@ -1,15 +1,38 @@
 package com.sh.module_251_control_ads_from_server_response;
 
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
 public class MainActivity extends AppCompatActivity {
 
+    AdView adView;
+    ProgressBar progressBar;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +43,62 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        adView = findViewById(R.id.banner_ad_view);
+        progressBar = findViewById(R.id.progressBar);
+
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(@NonNull InitializationStatus initializationStatus) {
+
+            }
+        });
+
+//        new Thread(() -> {
+//            MobileAds.initialize(this, initializationStatus -> {});
+//        })
+//        .start();
+
+
+
+
+
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://192.168.2.103/Apps/ad.php";
+
+        adView.setVisibility(GONE);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        if (response.contains("showAd")){
+
+                            adView.setVisibility(VISIBLE);
+                            progressBar.setVisibility(GONE);
+
+                            AdRequest adRequest = new AdRequest.Builder().build();
+                            adView.loadAd(adRequest);
+                        }
+                        else{
+                            adView.setVisibility(GONE);
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        queue.add(stringRequest);
+
+
+
+
     }
 }
